06ab9acd7d10cdb4ea8fa72c9095ff47709f9e5d
934781479 <934781479@qq.com>
2022-06-07 12:18:44 +0800

    6-712

--- _GuiClassHpp/clockDemo.hpp
+++ _GuiClassHpp/clockDemo.hpp
@@ -14,14 +14,12 @@ public:
     void setPara(vector<Cpx*>* vl, vector<Cpx*>* vr);
 protected:
     void timerEvent(QTimerEvent*) override;
-    void render2(QPainter* p);
     void render(QPainter* p) override;
 private:
     bool _bOk = false;
-    double _bx = 0;
-    double _by = 0;
     int _count = 0;
     QTime _beginTime;
+    QTime _clearTime;
     int m_timerId;
     QPainterPath* _sumPath = NULL;
     vector<Cpx*>* _vL;
